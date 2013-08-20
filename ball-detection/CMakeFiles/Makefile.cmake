9426b508d1d4492147f4829fc6633c442b7f3ea6
sagarrakshe <sagarrakshe2@gmail.com>
2013-08-20 14:50:05 +0530

    renamed file

--- ball-detection/CMakeFiles/Makefile.cmake
+++ ball-detection/CMakeFiles/Makefile.cmake
@@ -60,5 +60,5 @@ SET(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 SET(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/test.dir/DependInfo.cmake"
+  "CMakeFiles/detectBall.dir/DependInfo.cmake"
   )
